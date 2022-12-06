311139b8bbc9d38049369cd931de6ea0c5181bd8
CristinaaFlorea <floreacristina30@yahoo.ro>
2022-12-06 21:45:53 +0200

    finaal

--- client/problema.php
+++ client/problema.php
@@ -1,13 +1,11 @@
 <?php
     include("Conectare.php");
-    if ($result = $mysqli->query("SELECT * FROM tabelproduse where nume LIKE 'B%' AND price>max")) { 
+    if ($result = $mysqli->query("SELECT nume, MAX(pret) FROM tabelproduse where nume LIKE 'B%'")) { 
         if($result-> num_rows > 0){
             while($row = $result->fetch_object()){
-                echo "$row->nume";
+                echo  $row->nume ;
             }
-
         }
-    
     }
-?>
-       
+    $mysqli->close();
+?>  
